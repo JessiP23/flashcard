d886b3aed7414b760e257744034452dbf4de963f
JessiP23 <jessi316866@gmail.com>
2024-08-13 21:37:53 -0400

    handle submit logic for the flashcard

--- app/generate/page.js
+++ app/generate/page.js
@@ -9,6 +9,27 @@ export default function Generate() {
 
     const handleSubmit = async() => {
         // implete api call here
+        if (!text.trim()) {
+            alert('Please enter some text to generate flashcards.')
+            return
+        }
+
+        try {
+            const response = await fetch('/api/generate', {
+                method: 'POST',
+                body: text,
+            })
+
+            if (!response.ok) {
+                throw new Error('Failed to generate flashcards')
+            }
+
+            const data = await response.json()
+            setFlashcards(data)
+        } catch (error) {
+            console.error('Error generating flashcards', error)
+            alert('An error occured while generate flashcards. Please try again.')
+        }
     }
 
     return (
